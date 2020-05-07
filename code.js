efcfd74281476bd57f8d98f7641561b33cbe810b
ogdec327 <60277252+ogdec327@users.noreply.github.com>
2020-05-07 16:43:23 -0400

    fixes

--- code.js
+++ code.js
@@ -1,6 +1,32 @@
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.section');
+  
+
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
 
 var hotbod = document.querySelector("body");
-var $window = $("window");
 
 function doStuff() {
     hotbod.className += " animate";
@@ -10,31 +36,3 @@ window.onload = function() {
     doStuff();
 };
 
-$(window).scroll(function() {
-  
-  // selectors
-  var $body = $('body'),
-      $panel = $('.section');
-  
-  // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + ($window.height() / 3);
- 
-  $panel.each(function () {
-    var $this = $(this);
-    
-    // if position is within range of this panel.
-    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
-    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-          
-      // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-      });
-       
-      // Add class of currently active div
-      $body.addClass('color-' + $(this).data('color'));
-    }
-  });    
-  
-}).scroll();
